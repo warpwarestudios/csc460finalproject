3e04e1fda399d926af8f7fedaf9876ac600cf2fb
Raigir <Hammontreeryan@gmail.com>
2014-11-14 02:14:34 -0600

    Tinkered with databse

--- CSC460_BlackJack_Final_Burke_Hammontree_Smith/CSC460_BlackJack_Final_Burke_Hammontree_Smith/BlackjackMainWindow.Designer.cs
+++ CSC460_BlackJack_Final_Burke_Hammontree_Smith/CSC460_BlackJack_Final_Burke_Hammontree_Smith/BlackjackMainWindow.Designer.cs
@@ -28,19 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.databaseDataSet = new CSC460_BlackJack_Final_Burke_Hammontree_Smith.DatabaseDataSet();
+            this.playerBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.playerTableAdapter = new CSC460_BlackJack_Final_Burke_Hammontree_Smith.DatabaseDataSetTableAdapters.PlayerTableAdapter();
+            this.tableAdapterManager = new CSC460_BlackJack_Final_Burke_Hammontree_Smith.DatabaseDataSetTableAdapters.TableAdapterManager();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.databaseDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.playerBindingSource)).BeginInit();
             this.SuspendLayout();
+            // 
+            // databaseDataSet
+            // 
+            this.databaseDataSet.DataSetName = "DatabaseDataSet";
+            this.databaseDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // playerBindingSource
+            // 
+            this.playerBindingSource.DataMember = "Player";
+            this.playerBindingSource.DataSource = this.databaseDataSet;
+            // 
+            // playerTableAdapter
+            // 
+            this.playerTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.PlayerTableAdapter = this.playerTableAdapter;
+            this.tableAdapterManager.UpdateOrder = CSC460_BlackJack_Final_Burke_Hammontree_Smith.DatabaseDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(354, 146);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Test Insert Statement";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // BlackjackMainWindow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1113, 460);
+            this.ClientSize = new System.Drawing.Size(835, 374);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "BlackjackMainWindow";
             this.Text = "Blackjack";
+            ((System.ComponentModel.ISupportInitialize)(this.databaseDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.playerBindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private DatabaseDataSet databaseDataSet;
+        private System.Windows.Forms.BindingSource playerBindingSource;
+        private DatabaseDataSetTableAdapters.PlayerTableAdapter playerTableAdapter;
+        private DatabaseDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.Button button1;
     }
 }
--- CSC460_BlackJack_Final_Burke_Hammontree_Smith/CSC460_BlackJack_Final_Burke_Hammontree_Smith/BlackjackMainWindow.cs
+++ CSC460_BlackJack_Final_Burke_Hammontree_Smith/CSC460_BlackJack_Final_Burke_Hammontree_Smith/BlackjackMainWindow.cs
@@ -15,5 +15,31 @@ namespace CSC460_BlackJack_Final_Burke_Hammontree_Smith
         {
             InitializeComponent();
         }
+
+        private void playerBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.playerBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.databaseDataSet);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            // Test Insertion
+            System.Data.SqlClient.SqlConnection connect =
+            new System.Data.SqlClient.SqlConnection("Data Source=|DataDirectory|\\Database.sdf");
+
+            System.Data.SqlClient.SqlCommand cmd = new System.Data.SqlClient.SqlCommand();
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.CommandText = "INSERT Player (Username, Password, Bank, Borrowed) VALUES ('Ryan', 'fancypants', $100, '$0)";
+            cmd.Connection = connect;
+
+            connect.Open();
+            cmd.ExecuteNonQuery();
+            connect.Close();
+            // End Test Insertion
+        }
     }
 }
